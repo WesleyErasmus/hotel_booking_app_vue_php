5d4d60116148a6468a3ee97cd8627e2fec406cfe
Wesley Erasmus <wesley.erasmus119@gmail.com>
2022-12-08 19:52:51 +0200

    ProfilePage: Updated layout & created EditProfileDialog component

--- src/views/3_Profile/ProfilePage.vue
+++ src/views/3_Profile/ProfilePage.vue
@@ -1,67 +1,162 @@
 <template>
   <v-app id="app-content" class="mx-auto">
     <v-main class="profile-main">
-      <v-card class="profile-card-user-details">
-        <header>
-          <div class="profile-page-title-container">
-            <v-icon>mdi-account</v-icon>
-            <h1>Manage Profile</h1>
-          </div>
-        </header>
+      <div class="profile-card-user-details">
+        <div class="flex-header">
+          <header>
+            <h1 class="text-h2">Manage Profile</h1>
+            <div class="profile-page-title-container">
+              <v-icon class="account-icon">mdi-account</v-icon>
+              <!-- <h1 class="text-h2">Manage Profile</h1> -->
+              <div>
+                <h1 class="text-h4">Wesley Erasmus</h1>
+                <h1 class="text-h6">me@you.com</h1>
+              </div>
+            </div>
+          </header>
 
-        <section>
           <div class="user-information-disclaimer">
             Your information is safe with us and does not get sold or used for
-            Marketing. Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, deserunt!
+            Marketing. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Reprehenderit, deserunt!
           </div>
+        </div>
 
-          <v-form class="">
-            <h4>Have modal appear when edit gets clicked</h4>
-            <p>Update your profile information</p>
+        <section>
+          <div class="table-container">
+            <!-- <h4>Update your profile information</h4> -->
             <br />
-            <v-label>First Name</v-label>
-            <v-text-field variant="underlined">Wesley</v-text-field>
-            <v-label>Last Name</v-label>
-            <v-text-field variant="underlined">Erasmus</v-text-field>
-            <v-label>Email Address</v-label>
-            <v-text-field variant="underlined">wesley@me.com</v-text-field>
-            <v-label>Cellphone Number</v-label>
-            <v-text-field variant="underlined">+27835720741</v-text-field>
+            <v-table>
+              <td class="table-heading">Your account information</td>
+            </v-table>
+            <v-table class="">
+              <tbody>
+                <!-- name - surname - email - phone  -->
+                <tr>
+                  <td class="table-label">Name</td>
+                  <td>Wesley</td>
+                  <td>
+                    <div class="edit-btn" color="white">
+                      <EditProfileDialog />
+                    </div>
+                  </td>
+                </tr>
 
-            <v-btn class="edit-btn" color="black">Edit Your Information</v-btn>
-          </v-form>
+                <tr>
+                  <td class="table-label">Surname</td>
+                  <td>Erasmus</td>
+                  <td>
+                    <div class="edit-btn" color="white">
+                      <EditProfileDialog />
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td class="table-label">Email address</td>
+                  <td>me@you.com</td>
+                  <td>
+                    <div class="edit-btn" color="white">
+                      <EditProfileDialog />
+                    </div>
+                  </td>
+                </tr>
+
+                <tr>
+                  <td class="table-label">Phone number</td>
+                  <td>083 705 6273</td>
+                  <td>
+                    <div class="edit-btn" color="white">
+                      <EditProfileDialog />
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </div>
         </section>
-      </v-card>
+      </div>
     </v-main>
   </v-app>
 </template>
 
 <script>
-export default {};
+import EditProfileDialog from "./ProfilePageComponents/EditProfileDialog.vue";
+export default {
+  components: { EditProfileDialog },
+  data: () => ({
+    dialog: false,
+  }),
+};
 </script>
 
 <style scoped>
 .profile-main {
+  border: none !important;
 }
 .profile-page-title-container {
-    display: flex;
-    line-height: 1;
+  display: flex;
+  line-height: 1;
+  align-items: center;
+  margin-bottom: 3rem;
+}
+.flex-header {
+  display: flex;
+  justify-content: space-between;
+}
+.text-h2 {
+  margin-bottom: 2rem;
+}
+.account-icon {
+  font-size: 62px;
+  margin-right: 12px;
+  background: linear-gradient(
+    338deg,
+    rgba(242, 241, 239, 1) 0%,
+    rgba(254, 247, 234, 1) 43%,
+    rgba(222, 221, 219, 1) 100%
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  padding: 2.5rem;
+  border-radius: 50%;
+  border: 2px solid gold;
 }
 .user-information-disclaimer {
-    background: var(--transparent-bg);
-    padding: 2rem 1rem;
-    width: 450px;
-    border-radius: 10px;
-    margin: 2rem 0;
+  background: var(--transparent-bg);
+  padding: 2rem 1rem;
+  border-radius: 10px;
+  margin: 3rem 0;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  height: 150px;
+  width: 450px;
 }
 /* === V_CARD === */
 .profile-card-user-details {
-    width: 750px;
-    border: none;
-    padding: 4rem;
-    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  width: 750px;
+  border: none;
+  padding: 4rem;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  width: 70vw;
 }
 .edit-btn {
-    float: right;
+  float: right;
+}
+.table-container {
+}
+.table-heading {
+  background: rgba(226, 226, 226, 0.705);
+  display: block;
+  width: 100%;
+  padding: 0.5rem !important;
+  margin-bottom: 1rem;
+  border-radius: 5px;
+}
+td {
+  border-bottom: none !important;
+  padding: 1.5rem !important;
+}
+.table-label {
+  width: 200px;
 }
 </style>
---
+++ src/views/3_Profile/ProfilePageComponents/EditProfileDialog.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-row justify="center">
+    <v-dialog v-model="dialog" persistent>
+      <template v-slot:activator="{ props }">
+        <v-btn color="black" v-bind="props"
+          ><v-icon>mdi-pencil-box-multiple</v-icon> EDIT</v-btn
+        >
+      </template>
+      <v-card class="dialog-card">
+        <v-card-title>
+          <span class="text-h5">UPDATE YOUR PROFILE INFORMATION</span>
+        </v-card-title>
+        <v-container class="user-form-container">
+          <v-row>
+            <v-col cols="6">
+              <v-text-field required>Wesley</v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field persistent-hint required>Erasmus</v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field required>me@you.com</v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field type="number" required>083 705 6273</v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+        <small>*Click save to save your changes</small>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="black" variant="tonal" @click="dialog = false">
+            Close
+          </v-btn>
+          <v-btn color="black" variant="tonal" @click="dialog = false">
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+  }),
+};
+</script>
+
+<style scoped>
+.dialog-card {
+  display: block;
+  margin: 0 auto;
+  padding: 2rem 5rem;
+}
+
+.user-form-container {
+}
+
+.text-h5 {
+  font-weight: bold;
+}
+</style>
